cff856a108e3651a8acdb191040690bdc47a71fe
pimyn-girgis <bemenboshra2001@gmail.com>
2023-06-15 20:12:12 +0300

    Implemented most operators and multiple useful functions

--- dynamic_bitset.cpp
+++ dynamic_bitset.cpp
@@ -1,11 +1,11 @@
 #include "dynamic_bitset.hpp"
+#define VECTOR_SIZE(size) (size / bits_per_char + bool(size % bits_per_char))
 
-dynamic_bitset::reference::reference(dynamic_bitset& bitset, size_t index)
-    : m_byte{bitset.m_bits[index / bits_per_char]} {
-  m_bit = (char)(1 << (index % bits_per_char));
-}
+dynamic_bitset::reference::reference(dynamic_bitset &bitset, size_t index)
+    : m_byte{bitset.m_bits[index / bits_per_char]},
+      m_bit{char(1 << (index % bits_per_char))} {}
 
-dynamic_bitset::reference& dynamic_bitset::reference::operator=(bool value) {
+dynamic_bitset::reference &dynamic_bitset::reference::operator=(bool value) {
   if (value) {
     m_byte |= m_bit;
   } else {
@@ -14,24 +14,48 @@ dynamic_bitset::reference& dynamic_bitset::reference::operator=(bool value) {
   return *this;
 }
 
-dynamic_bitset::reference::operator bool() const {
-  return m_byte & m_bit;
-}
+dynamic_bitset::reference::operator bool() const { return m_byte & m_bit; }
+
+dynamic_bitset::dynamic_bitset() : m_size{0} {}
 
 dynamic_bitset::dynamic_bitset(size_t size)
-    : m_bits(size / bits_per_char + bool(size % bits_per_char)),
-      m_size(size) {}
+    : m_bits(VECTOR_SIZE(size)), m_size{size} {}
 
-size_t dynamic_bitset::size() const {
-  return m_size;
-}
+size_t dynamic_bitset::size() const { return m_size; }
 
 size_t dynamic_bitset::capacity() const {
   return m_bits.capacity() * bits_per_char;
 }
 
+bool dynamic_bitset::all() const {
+  return m_size == 0 ||
+         m_bits.back() == 1 << (m_size % 8 - 1) &&
+             for_each(m_bits.begin(), m_bits.end() - 1,
+                      [](unsigned char byte) { return byte == 0xFF; });
+}
+
+bool dynamic_bitset::none() const {
+  return m_size == 0 ||
+         for_each(m_bits.begin(), m_bits.end(),
+                  [](unsigned char byte) { return byte == 0x00; });
+}
+
+bool dynamic_bitset::any() const { return !none(); }
+
+size_t dynamic_bitset::count() const {
+  size_t count{0};
+  for (size_t i{0}; i < m_size; ++i) {
+    count += (*this)[i];
+  }
+  return count;
+}
+
+void dynamic_bitset::flip(size_t index) { (*this)[index] = !(*this)[index]; }
+
 void dynamic_bitset::resize(size_t size) {
-  m_bits.resize(size / bits_per_char + bool(size % bits_per_char));
+  m_bits.resize(VECTOR_SIZE(size));
+  m_bits.back() &=
+      (1 << (size % 8)) - 1; // important for none() to function properly
   m_size = size;
 }
 
@@ -41,20 +65,87 @@ void dynamic_bitset::clear() {
 }
 
 dynamic_bitset::reference dynamic_bitset::operator[](size_t index) {
+  if (index >= m_size) {
+    throw std::out_of_range("Index out of range");
+  }
   return {*this, index};
 }
 
 const dynamic_bitset::reference dynamic_bitset::operator[](size_t index) const {
-  return {*const_cast<dynamic_bitset*>(this), index};
+  return const_cast<dynamic_bitset &>(*this)[index];
 }
 
-std::ostream& operator<<(std::ostream& os, const dynamic_bitset::reference& bit) {
+std::ostream &operator<<(std::ostream &os,
+                         const dynamic_bitset::reference &bit) {
   return os << (bool)bit;
 }
 
-std::ostream& operator<<(std::ostream& os, const dynamic_bitset& bitset) {
-  for (size_t i{0}; i < bitset.m_size; ++i) {
-    os << bitset[i];
+std::ostream &operator<<(std::ostream &os, const dynamic_bitset &bitset) {
+  for (size_t i{bitset.m_size}; i > 0; --i) {
+    os << bitset[i - 1];
   }
   return os;
 }
+
+dynamic_bitset dynamic_bitset::operator=(const std::string &bitstring) {
+  resize(bitstring.size());
+  for (size_t i{0}; i < m_size; ++i) {
+    (*this)[i] = bitstring[m_size - i - 1] != '0';
+  }
+  return *this;
+}
+
+dynamic_bitset dynamic_bitset::operator=(const unsigned int integer) {
+  resize(sizeof(integer) * 8);
+  memcpy(m_bits.data(), &integer, sizeof(integer));
+  return *this;
+}
+
+bool dynamic_bitset::operator==(const dynamic_bitset &bitset) const {
+  return m_size == bitset.m_size &&
+         !memcmp(m_bits.data(), bitset.m_bits.data(), VECTOR_SIZE(m_size));
+}
+
+dynamic_bitset dynamic_bitset::operator~() const {
+  dynamic_bitset result(m_size);
+  for (size_t i{0}; i < VECTOR_SIZE(m_size); ++i) {
+    result.m_bits[i] = ~m_bits[i];
+  }
+  return result;
+}
+
+dynamic_bitset dynamic_bitset::operator&(const dynamic_bitset &bitset) const {
+  dynamic_bitset result(m_size);
+  for (size_t i{0}; i < VECTOR_SIZE(m_size); ++i) {
+    result.m_bits[i] = m_bits[i] & bitset.m_bits[i];
+  }
+  return result;
+}
+
+dynamic_bitset dynamic_bitset::operator|(const dynamic_bitset &bitset) const {
+  dynamic_bitset result(m_size);
+  for (size_t i{0}; i < VECTOR_SIZE(m_size); ++i) {
+    result.m_bits[i] = m_bits[i] | bitset.m_bits[i];
+  }
+  return result;
+}
+
+dynamic_bitset dynamic_bitset::operator^(const dynamic_bitset &bitset) const {
+  dynamic_bitset result(m_size);
+  for (size_t i{0}; i < VECTOR_SIZE(m_size); ++i) {
+    result.m_bits[i] = m_bits[i] ^ bitset.m_bits[i];
+  }
+  return result;
+}
+
+dynamic_bitset dynamic_bitset::operator<<(size_t shift) const {
+  dynamic_bitset result(m_size);
+  return result;
+}
+
+dynamic_bitset dynamic_bitset::operator>>(size_t shift) const {
+  dynamic_bitset result(m_size);
+  return result;
+}
+
+#undef VECTOR_SIZE
--- dynamic_bitset.hpp
+++ dynamic_bitset.hpp
@@ -1,6 +1,10 @@
 #ifndef DYNAMIC_BITSET_HPP
 #define DYNAMIC_BITSET_HPP
 
+#include <algorithm>
+#include <cstdlib>
+#include <cstring>
+#include <exception>
 #include <istream>
 #include <ostream>
 #include <vector>
@@ -12,26 +16,43 @@ private:
   size_t m_size;
   class reference {
   private:
-    unsigned char& m_byte;
+    unsigned char &m_byte;
     char m_bit;
 
   public:
-    reference(dynamic_bitset& bitset, size_t index);
-    reference& operator=(bool value);
-    explicit operator bool() const;
+    reference(dynamic_bitset &bitset, size_t index);
+    reference &operator=(bool value);
+    operator bool() const;
   };
 
 public:
+  explicit dynamic_bitset();
   explicit dynamic_bitset(size_t size);
-  [[nodiscard]] size_t size() const;
-  [[nodiscard]] size_t capacity() const;
-  void resize(size_t size);
-  void clear();
-  friend std::ostream& operator<<(std::ostream& os,
-                                  const dynamic_bitset& bitset);
-  friend std::ostream& operator<<(std::ostream &os, const dynamic_bitset::reference &bit);
+  [[nodiscard]] size_t size() const; // returns the number of bits
+  [[nodiscard]] size_t capacity() const; // returns the capacity of the underlying vector
+  [[nodiscard]] bool all() const; // returns true if all bits are set or if the bitset is empty
+  [[nodiscard]] bool none() const; // returns true if no bits are set or if the bitset is empty
+  [[nodiscard]] bool any() const;     // returns true if not none()
+  [[nodiscard]] size_t count() const; // returns the number of set bits
+  void flip(size_t index);            // flips the bit at the given index
+  void resize(size_t size);           // resizes the bitset to the given size
+  void clear();                       // clears the bitset
+  friend std::ostream &operator<<(std::ostream &os,
+                                  const dynamic_bitset &bitset);
+  friend std::ostream &operator<<(std::ostream &os,
+                                  const dynamic_bitset::reference &bit);
+  dynamic_bitset operator=(const std::string &bitstring);
+  dynamic_bitset operator=(const unsigned int);
+  dynamic_bitset operator~() const;
+  dynamic_bitset operator&(const dynamic_bitset &bitset) const;
+  dynamic_bitset operator|(const dynamic_bitset &bitset) const;
+  dynamic_bitset operator^(const dynamic_bitset &bitset) const;
+  dynamic_bitset operator<<(size_t shift) const; // to be defined
+  dynamic_bitset operator>>(size_t shift) const; // to be defined
+  bool operator==(const dynamic_bitset &bitset) const;
+
   reference operator[](size_t index);
   const reference operator[](size_t index) const;
 };
 
-#endif  //DYNAMIC_BITSET_HPP
+#endif // DYNAMIC_BITSET_HPP
